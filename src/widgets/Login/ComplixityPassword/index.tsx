ca1411323b4fba3545cef8d7b84b6072f20e98a9
NilsenBor <iliyasav18@gmail.com>
2024-05-14 22:07:46 +0300

    feat: add complixity of password

--- src/widgets/Login/ComplixityPassword/index.tsx
+++ src/widgets/Login/ComplixityPassword/index.tsx
@@ -1,74 +1,17 @@
-"use client";
-
-import { checkComplixityPassword, isPassword } from "@/features/api/util/form";
-import { useState } from "react";
+import React from "react"; // Import React if not already imported
+import { checkComplixityPassword } from "@/features/api/util/form";
 
 const InputPasswordLabel = () => {
-  const [inputValue, setInputValue] = useState("");
-  const [isFocused, setIsFocused] = useState(false);
-
-  const handleInputChange = (e) => {
-    setInputValue(e.target.value);
-    if (e.target.value == "") {
-      setIsFocused(false);
-    } else {
-      setIsFocused(true);
-    }
-  };
-
-  const handleLabelClick = () => {
-    setIsFocused(true);
-  };
-
-  const handleInputBlur = () => {
-    if (inputValue === "") {
-      setIsFocused(false);
-    }
-  };
+  const text = "111_hello_123132167";
 
   return (
-    <div className="relative ">
-      <input
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        onFocus={handleLabelClick}
-        onBlur={handleInputBlur}
-        className={`m-2 w-48 h-8 border rounded-lg p-2 pl-4 transition-all duration-300 focus:w-48 focus:top-0 focus-left-0 absolute border-black focus:border-blue-500 focus:border-2 
-                ${checkComplixityPassword(inputValue) == "Weak" && inputValue.length > 0 ? " focus:border-red-500 border-red-500 text-red-500" : ""} 
-                ${checkComplixityPassword(inputValue) == "Medium" && inputValue.length > 0 ? " focus:border-yellow-500 border-yellow-500 text-yellow-500" : ""} 
-                ${checkComplixityPassword(inputValue) == "Strong" && inputValue.length > 0 ? " focus:border-green-500 border-green-500 text-green-500" : ""}  
-                ${inputValue.length > 0 ? "focus:border-blue-500 focus:border-2.5" : ""}
-                ${isPassword(inputValue) == false ? " text-white" : ""}`}
-        placeholder=""
-      />
-      <label
-        className={`absolute top-3 left-4 text-gray-700 transform duration-100 
-                ${checkComplixityPassword(inputValue) == "Weak" && inputValue.length > 0 ? " focus:border-red-500 border-red-500 text-red-500" : ""} 
-                ${checkComplixityPassword(inputValue) == "Medium" && inputValue.length > 0 ? " focus:border-yellow-500 border-yellow-500 text-yellow-500" : ""} 
-                ${checkComplixityPassword(inputValue) == "Strong" && inputValue.length > 0 ? " focus:border-green-500 border-green-500 text-green-500" : ""} 
-                ${inputValue != "" || isFocused == true ? "-translate-y-3.5 text-xs bg-white text-blue-600 " : "pointer-events-none "}`}
-      >
-        Password
-      </label>
-      <label
-        className={`text-xs absolute top-10 left-4
-                ${checkComplixityPassword(inputValue) == "Weak" && inputValue.length > 0 ? "text-red-500" : "hidden"} `}
-      >
-        Password is weak
-      </label>
-      <label
-        className={`text-xs absolute top-10 left-4
-                ${checkComplixityPassword(inputValue) == "Medium" && inputValue.length > 0 ? "text-yellow-500" : "hidden"} `}
-      >
-        Password is medium
-      </label>
-      <label
-        className={`text-xs absolute top-10 left-4
-                ${checkComplixityPassword(inputValue) == "Strong" && inputValue.length > 0 ? "text-green-500" : "hidden"} `}
-      >
-        Password is strong
-      </label>
+    <div className="relative w-60 h-8 border-2  border-desaturated-cyan rounded-lg">
+      <div
+        className={`absolute w-full h-full 
+                ${checkComplixityPassword(text) === "Weak" && text.length > 0 ? "bg-red-500 w-1/3" : ""} 
+                ${checkComplixityPassword(text) === "Medium" && text.length > 0 ? "bg-yellow-500 w-2/3" : ""} 
+                ${checkComplixityPassword(text) === "Strong" && text.length > 0 ? "bg-green-500" : ""} `}
+      ></div>
     </div>
   );
 };
