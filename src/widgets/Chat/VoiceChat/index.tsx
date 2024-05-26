c648fcfcb52601df096c3c4417b21b61acd2a706
NilsenBor <iliyasav18@gmail.com>
2024-05-26 14:44:59 +0300

    feat: need some works - new color or data

---
+++ src/widgets/Chat/VoiceChat/index.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import Userlogo from "../Userlogo/Userlogo";
+
+function VoiceChat() {
+  const [time, setTime] = useState(0);
+  const [buttonStates, setButtonStates] = useState({
+    Voice: false,
+    Screen: false,
+    Micro: false,
+    Telephone: false,
+  });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime((prevTime) => prevTime + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const formattedTime = new Date(time * 1000).toISOString().substr(14, 5);
+
+  const handleButtonPress = (buttonName) => {
+    setButtonStates((prevStates) => ({
+      ...prevStates,
+      [buttonName]: !prevStates[buttonName],
+    }));
+  };
+
+  return (
+    <div className="flex flex-col h-screen w-screen bg-msu-green">
+      <div className="h-auto mt-4 flex">
+        <div className="w-2/3 ml-6 text-verdigris">Никита Ефремов</div>
+        <div className="flex w-1/3 justify-end space-x-4 mr-6">
+          <button>
+            <img
+              src="/assets/icon/Voice-icon.svg"
+              alt="off/on voice"
+              className="w-6 h-6 mx-2"
+            />
+          </button>
+          <button>
+            <img
+              src="/assets/icon/Add-user-icon.svg"
+              alt="add user"
+              className="w-6 h-6 mx-2"
+            />
+          </button>
+        </div>
+      </div>
+      <div className="flex-1 flex flex-col items-center justify-center">
+        <div>
+          <div className="border rounded-full m-6 border-green-300 inline-block w-32 h-32">
+            <Userlogo />
+          </div>
+          <div className="border rounded-full m-6 border-green-300 inline-block w-32 h-32">
+            <Userlogo />
+          </div>
+        </div>
+        <div className="text-center text-gray-500 text-lg">{formattedTime}</div>
+      </div>
+      <div className="w-full h-18 my-6 flex items-center justify-center space-x-8">
+        <button
+          className={`transition-all duration-150 w-14 h-14 flex items-center justify-center rounded-full ${buttonStates.Voice ? "bg-police-blue" : "bg-desaturated-cyan"} `}
+          onClick={() => handleButtonPress("Voice")}
+        >
+          <img
+            src="/assets/icon/Voice-Call-icon.svg"
+            alt="no video call"
+            className="w-8 h-8 mx-auto"
+          />
+        </button>
+        <button
+          className={`transition-all duration-150 w-14 h-14 flex items-center justify-center rounded-full ${buttonStates.Screen ? "bg-police-blue" : "bg-desaturated-cyan"} `}
+          onClick={() => handleButtonPress("Screen")}
+        >
+          <img
+            src="/assets/icon/Screen-icon.svg"
+            alt="screen"
+            className="w-8 h-8 mx-auto"
+          />
+        </button>
+        <button
+          className={`transition-all duration-150 w-14 h-14 flex items-center justify-center rounded-full ${buttonStates.Micro ? "bg-police-blue" : "bg-desaturated-cyan"} `}
+          onClick={() => handleButtonPress("Micro")}
+        >
+          <img
+            src="/assets/icon/Micro-icon.svg"
+            alt="Off/on micro"
+            className="w-6 h-6 mx-auto"
+          />
+        </button>
+        <button
+          className={`transition-all duration-150 w-14 h-14 flex items-center justify-center rounded-full ${buttonStates.Telephone ? "bg-police-blue" : "bg-red-500"} `}
+          onClick={() => handleButtonPress("Telephone")}
+        >
+          <img
+            src="/assets/icon/Telephone-icon.svg"
+            alt="end the call"
+            className="w-8 h-8 mx-auto"
+          />
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default VoiceChat;
---
+++ src/widgets/ContextMenu/index.tsx
@@ -0,0 +1,56 @@
+function ContextMenu() {
+  return (
+    <div className="w-52 h-64  ">
+      <div className="pb-6 transition-all duration-150 border-b-2 border-gray-800 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-2 font-medium hover:bg-desaturated-cyan rounded-t-xl">
+        <img
+          src="/assets/icon/Open-In-New-Window.svg"
+          alt="Open in new window"
+          className="w-4 h-4 mr-1"
+        />
+        Открыть в отдельном окне
+      </div>
+      <button className="transition-all duration-150 pb-6 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan">
+        <img
+          src="/assets/icon/Archive.svg"
+          alt="Archive"
+          className="h-4 w-4 mr-1"
+        />
+        Архивировать
+      </button>
+      <button className="transition-all duration-150 pb-6 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan">
+        <img
+          src="/assets/icon/Secure.svg"
+          alt="Secure"
+          className="h-4 w-4 mr-1"
+        />
+        Закрепить
+      </button>
+      <button className="transition-all duration-150 pb-6 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan">
+        <img
+          src="/assets/icon/Off-Volume.svg"
+          alt="Off volume"
+          className="h-4 w-4 mr-1"
+        />
+        Выключить уведомления
+      </button>
+      <button className="transition-all duration-150 pb-6 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan">
+        <img
+          src="/assets/icon/Mark-as-read.svg"
+          alt="Archive"
+          className="h-4 w-4 mr-1"
+        />
+        Пометить как прочитанное
+      </button>
+      <button className="transition-all duration-150 pb-6 h-6 w-full text-red-600 text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan rounded-b-xl">
+        <img
+          src="/assets/icon/Leave-the-channel.svg"
+          alt="leave the channel"
+          className="h-4 w-4 mr-1"
+        />
+        Покинуть канал
+      </button>
+    </div>
+  );
+}
+
+export default ContextMenu;
