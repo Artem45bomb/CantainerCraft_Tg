a3f8c2ada552f9b60304cebaa5a8aa3989fe4e27
NilsenBor <iliyasav18@gmail.com>
2024-05-27 21:08:21 +0300

    feat: remade img to Image

--- src/widgets/Chat/VoiceChat/index.tsx
+++ src/widgets/Chat/VoiceChat/index.tsx
@@ -3,6 +3,7 @@
 import { useEffect, useRef, useState } from "react";
 import Userlogo from "../Userlogo/Userlogo";
 import { ObjectFields } from "@/features/api/util";
+import Image from "next/image";
 
 function VoiceChat() {
   const [time, setTime] = useState(0);
@@ -24,23 +25,23 @@ function VoiceChat() {
     return () => clearInterval(interval);
   }, []);
 
-  const videoStart = (isActiveAudio:boolean) => {
+  const videoStart = (isActiveAudio: boolean) => {
     navigator.mediaDevices
-    .getUserMedia({ video: true, audio: isActiveAudio })
-    .then((stream) => {
-      if (videoUserRef.current) {
-        videoUserRef.current.srcObject = stream;
-        videoUserRef.current.play();
-      }
-      if (videoIncomingRef.current) {
-      videoIncomingRef.current.srcObject = stream;
-      videoIncomingRef.current.play();
-    }
-    })
-    .catch((err) => {
-      console.error(`An error occurred: ${err}`);
-    });
-  }
+      .getUserMedia({ video: true, audio: isActiveAudio })
+      .then((stream) => {
+        if (videoUserRef.current) {
+          videoUserRef.current.srcObject = stream;
+          videoUserRef.current.play();
+        }
+        if (videoIncomingRef.current) {
+          videoIncomingRef.current.srcObject = stream;
+          videoIncomingRef.current.play();
+        }
+      })
+      .catch((err) => {
+        console.error(`An error occurred: ${err}`);
+      });
+  };
 
   const videoStop = () => {
     if (videoUserRef.current) {
@@ -51,7 +52,7 @@ function VoiceChat() {
       videoIncomingRef.current.pause();
       videoIncomingRef.current.srcObject = null;
     }
-  }
+  };
 
   const formattedTime = new Date(time * 1000).toISOString().substr(14, 5);
 
@@ -67,38 +68,39 @@ function VoiceChat() {
       <div className="h-auto mt-4 flex">
         <div className="w-2/3 ml-6 text-verdigris">Никита Ефремов</div>
         <div className="flex w-1/3 justify-end space-x-4 mr-6">
-          <button>
-            <img
+          <button className="relative w-6 h-6 mx-2">
+            <Image
               src="/assets/icon/Voice-icon.svg"
               alt="off/on voice"
-              className="w-6 h-6 mx-2"
-            />
-          </button>
-          <button>
-            <img
-              src="/assets/icon/Add-user-icon.svg"
-              alt="add user"
-              className="w-6 h-6 mx-2"
+              layout="fill"
             />
           </button>
         </div>
       </div>
       <div className="flex-1 flex flex-col items-center justify-center">
-        {settings.Voice == false ? <div>
-          <div className="border rounded-full m-6 border-green-300 inline-block w-32 h-32">
-            <Userlogo />
-          </div>
-          <div className="border rounded-full m-6 border-green-300 inline-block w-32 h-32">
-            <Userlogo />
-          </div>
-        </div> :
-        <div className="w-full h-2/4 mb-6">
-            <div className="flex w-full gap-5 h-full justify-center">
-              <video className="h-full rounded-xl border-2 border-dark-50" ref={videoUserRef}></video>
-              <video className="h-full rounded-xl border-2 border-dark-50" ref={videoIncomingRef}></video>
+        {settings.Voice == false ? (
+          <div>
+            <div className="border rounded-full m-6 border-green-300 inline-block w-32 h-32">
+              <Userlogo />
             </div>
-        </div>
-        }
+            <div className="border rounded-full m-6 border-green-300 inline-block w-32 h-32">
+              <Userlogo />
+            </div>
+          </div>
+        ) : (
+          <div className="w-full h-2/4 mb-6">
+            <div className="flex w-full gap-5 h-full justify-center">
+              <video
+                className="h-full rounded-xl border-2 border-dark-50"
+                ref={videoUserRef}
+              ></video>
+              <video
+                className="h-full rounded-xl border-2 border-dark-50"
+                ref={videoIncomingRef}
+              ></video>
+            </div>
+          </div>
+        )}
         <div className="text-center text-gray-500 text-lg">{formattedTime}</div>
       </div>
       <div className="w-full h-18 my-6 flex items-center justify-center space-x-8">
@@ -106,28 +108,29 @@ function VoiceChat() {
           className={`transition-all duration-150 w-14 h-14 flex items-center justify-center rounded-full ${settings.Voice ? "bg-police-blue" : "bg-desaturated-cyan"} `}
           onClick={() => {
             handleButtonPress("Voice");
-            if (!settings.Voice)
-              videoStart(true);
-            else
-              videoStop();
+            if (!settings.Voice) videoStart(true);
+            else videoStop();
           }}
-          
         >
-          <img
-            src="/assets/icon/Voice-Call-icon.svg"
-            alt="no video call"
-            className="w-8 h-8 mx-auto"
-          />
+          <div className="relative w-8 h-8 mx-auto">
+            <Image
+              layout="fill"
+              src="/assets/icon/Voice-Call-icon.svg"
+              alt="no video call"
+            />
+          </div>
         </button>
         <button
           className={`transition-all duration-150 w-14 h-14 flex items-center justify-center rounded-full ${settings.Screen ? "bg-police-blue" : "bg-desaturated-cyan"} `}
           onClick={() => handleButtonPress("Screen")}
         >
-          <img
-            src="/assets/icon/Screen-icon.svg"
-            alt="screen"
-            className="w-8 h-8 mx-auto"
-          />
+          <div className="relative w-8 h-8 mx-auto">
+            <Image
+              src="/assets/icon/Screen-icon.svg"
+              alt="screen"
+              layout="fill"
+            />
+          </div>
         </button>
         <button
           className={`transition-all duration-150 w-14 h-14 flex items-center justify-center rounded-full ${settings.Micro ? "bg-police-blue" : "bg-desaturated-cyan"} `}
@@ -136,21 +139,25 @@ function VoiceChat() {
             videoStart(settings.Micro);
           }}
         >
-          <img
-            src="/assets/icon/Micro-icon.svg"
-            alt="Off/on micro"
-            className="w-6 h-6 mx-auto"
-          />
+          <div className="relative w-6 h-6 mx-auto">
+            <Image
+              src="/assets/icon/Micro-icon.svg"
+              alt="Off/on micro"
+              layout="fill"
+            />
+          </div>
         </button>
         <button
-          className={`transition-all duration-150 w-14 h-14 flex items-center justify-center rounded-full ${settings.Telephone ? "bg-police-blue" : "bg-red-500"} `}
+          className={`relative transition-all duration-150 w-14 h-14 flex items-center justify-center rounded-full ${settings.Telephone ? "bg-police-blue" : "bg-red-500"} `}
           onClick={() => handleButtonPress("Telephone")}
         >
-          <img
-            src="/assets/icon/Telephone-icon.svg"
-            alt="end the call"
-            className="w-8 h-8 mx-auto"
-          />
+          <div className="relative w-10 h-10">
+            <Image
+              layout="fill"
+              src="/assets/icon/Telephone-icon.svg"
+              alt="end the call"
+            />
+          </div>
         </button>
       </div>
     </div>
--- src/widgets/ContextMenu/index.tsx
+++ src/widgets/ContextMenu/index.tsx
@@ -1,8 +1,11 @@
+"use client";
+import Image from "next/image";
 function ContextMenu() {
   return (
     <div className="w-52 h-64  ">
       <div className="pb-6 transition-all duration-150 border-b-2 border-gray-800 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-2 font-medium hover:bg-desaturated-cyan rounded-t-xl">
-        <img
+        <Image
+          layout="fill"
           src="/assets/icon/Open-In-New-Window.svg"
           alt="Open in new window"
           className="w-4 h-4 mr-1"
@@ -10,7 +13,8 @@ function ContextMenu() {
         Открыть в отдельном окне
       </div>
       <button className="transition-all duration-150 pb-6 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan">
-        <img
+        <Image
+          layout="fill"
           src="/assets/icon/Archive.svg"
           alt="Archive"
           className="h-4 w-4 mr-1"
@@ -18,7 +22,8 @@ function ContextMenu() {
         Архивировать
       </button>
       <button className="transition-all duration-150 pb-6 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan">
-        <img
+        <Image
+          layout="fill"
           src="/assets/icon/Secure.svg"
           alt="Secure"
           className="h-4 w-4 mr-1"
@@ -26,7 +31,8 @@ function ContextMenu() {
         Закрепить
       </button>
       <button className="transition-all duration-150 pb-6 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan">
-        <img
+        <Image
+          layout="fill"
           src="/assets/icon/Off-Volume.svg"
           alt="Off volume"
           className="h-4 w-4 mr-1"
@@ -34,7 +40,8 @@ function ContextMenu() {
         Выключить уведомления
       </button>
       <button className="transition-all duration-150 pb-6 h-6 w-full text-msu-green text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan">
-        <img
+        <Image
+          layout="fill"
           src="/assets/icon/Mark-as-read.svg"
           alt="Archive"
           className="h-4 w-4 mr-1"
@@ -42,7 +49,8 @@ function ContextMenu() {
         Пометить как прочитанное
       </button>
       <button className="transition-all duration-150 pb-6 h-6 w-full text-red-600 text-xs flex bg-crystal justify-start pl-3 pt-1 font-medium hover:bg-desaturated-cyan rounded-b-xl">
-        <img
+        <Image
+          layout="fill"
           src="/assets/icon/Leave-the-channel.svg"
           alt="leave the channel"
           className="h-4 w-4 mr-1"
