c4a0d3e6ad295075da1cb7ec2b86d1a7008e0103
geralttop <bluevotinamalibu@gmail.com>
2024-07-14 18:47:16 +0300

    feat(pill): i have a problems with switch and range, and i installed material-tailwind

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,13 +1,14 @@
 "use client";
+
 import { Chat } from "@/widgets/Chat";
-import ContextMenu from "@/widgets/ContextMenu";
+import SettingsPanel from "@/widgets/SettingsPanel";
 
 export default function TestPage() {
   //const [active, setActive] = useState<boolean>(false);
 
   return (
     <div className="w-full h-full bg-black">
-      <Chat />
+      <SettingsPanel />
     </div>
   );
 }
--- src/widgets/SettingsPanel/index.tsx
+++ src/widgets/SettingsPanel/index.tsx
@@ -1,24 +1,32 @@
 import Image from "next/image";
+import { Slider } from "@material-tailwind/react";
 
 export default function () {
   return (
     <>
-      <header className="w-104 bg-police-blue">
-        <div className=" m-8">
+      <header className="w-104 bg-1b5155 ">
+        <div className=" mx-8">
           <div className=" flex justify-between pt-4">
             <div>
               <p className=" text-white text-2xl font-semibold">Настройки</p>
             </div>
             <div>
-              <button>
+              <button className=" mr-6">
                 <Image
-                  src={"/assets/testIcons/weather.svg"}
+                  src={"/assets/testIcons/threeDots.png"}
                   width={18}
-                  height={18}
+                  height={20}
                   alt="3 dots"
                 />
               </button>
-              <button>X</button>
+              <button>
+                <Image
+                  src={"/assets/testIcons/cross.png"}
+                  width={20}
+                  height={23}
+                  alt="cross"
+                />
+              </button>
             </div>
           </div>
           <div className="flex mt-5 pb-4">
@@ -41,6 +49,179 @@ export default function () {
           </div>
         </div>
       </header>
+      <div className=" h-3 bg-msu-green w-104"></div>
+      <main className="w-104 bg-1b5155 ">
+        <div className=" mx-8">
+          <ul>
+            <li>
+              <button className="flex py-3">
+                <Image
+                  src={"/assets/testIcons/account.svg"}
+                  width={18}
+                  height={18}
+                  alt="account"
+                />
+                <p className=" ml-3 text-white text-base font-semibold ">
+                  Мой аккаунт
+                </p>
+              </button>
+            </li>
+            <li>
+              <button className="flex py-3">
+                <Image
+                  src={"/assets/testIcons/lock.svg"}
+                  width={18}
+                  height={18}
+                  alt="account"
+                />
+                <p className=" ml-3 text-white text-base font-semibold ">
+                  Конфиденциальность
+                </p>
+              </button>
+            </li>
+            <li>
+              <button className="flex py-3">
+                <Image
+                  src={"/assets/testIcons/ring.svg"}
+                  width={18}
+                  height={18}
+                  alt="account"
+                />
+                <p className=" ml-3 text-white text-base font-semibold ">
+                  Уведомления и звуки
+                </p>
+              </button>
+            </li>
+            <li>
+              <button className="flex py-3">
+                <Image
+                  src={"/assets/testIcons/message.svg"}
+                  width={18}
+                  height={18}
+                  alt="account"
+                />
+                <p className=" ml-3 text-white text-base font-semibold ">
+                  Настройка чатов
+                </p>
+              </button>
+            </li>
+            <li>
+              <button className="flex py-3">
+                <Image
+                  src={"/assets/testIcons/folder.svg"}
+                  width={18}
+                  height={18}
+                  alt="account"
+                />
+                <p className=" ml-3 text-white text-base font-semibold ">
+                  Папки с чатами
+                </p>
+              </button>
+            </li>
+            <li>
+              <button className="flex py-3">
+                <Image
+                  src={"/assets/testIcons/gears.svg"}
+                  width={18}
+                  height={18}
+                  alt="account"
+                />
+                <p className=" ml-3 text-white text-base font-semibold ">
+                  Продвинутые настройки
+                </p>
+              </button>
+            </li>
+            <li>
+              <button className="flex py-3">
+                <Image
+                  src={"/assets/testIcons/volume.svg"}
+                  width={18}
+                  height={18}
+                  alt="account"
+                />
+                <p className=" ml-3 text-white text-base font-semibold ">
+                  Звуки и камера
+                </p>
+              </button>
+            </li>
+            <li>
+              <button className="flex py-3">
+                <Image
+                  src={"/assets/testIcons/battery.svg"}
+                  width={18}
+                  height={18}
+                  alt="account"
+                />
+                <p className=" ml-3 text-white text-base font-semibold ">
+                  Заряд батареи и анимация
+                </p>
+              </button>
+            </li>
+            <li>
+              <button className="flex justify-between py-3 w-90">
+                <div className="flex ">
+                  <Image
+                    src={"/assets/testIcons/language.svg"}
+                    width={18}
+                    height={18}
+                    alt="account"
+                  />
+                  <p className=" ml-3 text-white text-base font-semibold ">
+                    Язык
+                  </p>
+                </div>
+                <p className=" text-base text-#9FDAD6 font-semibold opacity-55">
+                  Русский
+                </p>
+              </button>
+            </li>
+            <li>
+              <button className="flex py-3">
+                <Image
+                  src={"/assets/testIcons/weather.svg"}
+                  width={18}
+                  height={18}
+                  alt="account"
+                />
+                <p className=" ml-3 text-white text-base font-semibold ">
+                  Погода
+                </p>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <div className=" h-3 bg-msu-green w-104"></div>
+      <div className="w-104 bg-1b5155 ">
+        <div className="mx-8">
+          <div className="flex justify-between py-3 w-90">
+            <div className="flex ">
+              <Image
+                src={"/assets/testIcons/language.svg"}
+                width={18}
+                height={18}
+                alt="account"
+              />
+              <p className=" ml-3 text-white text-base font-semibold ">
+                Масштаб по умолчанию
+              </p>
+            </div>
+            <input type="checkbox" />
+          </div>
+          <input
+            type="range"
+            id="volume"
+            min="0"
+            max="11"
+            value="7"
+            step="1"
+            className=" w-86"
+          />
+          {/* <Slider defaultValue={100}/> */}
+        </div>
+      </div>
+      <div></div>
+      <div className=" h-3 bg-msu-green w-104"></div>
     </>
   );
 }
